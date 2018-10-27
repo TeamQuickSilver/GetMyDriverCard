4e04064a64c545caea1d3410cdca8da961522cc6
tevetely <cvetygetova@yahoo.com>
2018-10-27 09:39:56 +0300

    fagment_new_card.xml
    Added layout_marginStart to EditText

---
+++ GetMyDriverCardAndroid/app/src/main/java/com/quicksilver/getmydrivercard/views/step3/CameraUtils.java
@@ -0,0 +1,14 @@
+package com.quicksilver.getmydrivercard.views.step3;
+
+import android.content.Context;
+
+public class CameraUtils {
+
+    public static boolean isDeviceSupportCamera(Context applicationContext) {
+        return false;
+    }
+
+    public static boolean checkPerissions(Context applicationContext) {
+        return false;
+    }
+}
