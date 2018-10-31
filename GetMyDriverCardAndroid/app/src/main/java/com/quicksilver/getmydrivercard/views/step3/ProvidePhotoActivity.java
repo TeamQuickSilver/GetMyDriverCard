aff55238acd9265d4d9048b0f1da6e019e582dad
tevetely <cvetygetova@yahoo.com>
2018-10-31 13:21:06 +0200

    ProvidePhotoActivity

--- GetMyDriverCardAndroid/app/src/main/java/com/quicksilver/getmydrivercard/views/step3/ProvidePhotoActivity.java
+++ GetMyDriverCardAndroid/app/src/main/java/com/quicksilver/getmydrivercard/views/step3/ProvidePhotoActivity.java
@@ -1,16 +1,33 @@
 package com.quicksilver.getmydrivercard.views.step3;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import com.quicksilver.getmydrivercard.R;
 
-public class ProvidePhotoActivity extends Activity {
+import java.io.File;
+import java.util.List;
 
-    private static final int CAMERA_CAPTURE_IMEGE_REQUEST_CODE = 100;
+public class ProvidePhotoActivity extends AppCompatActivity {
+
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
     private static String storagePath;
 
     @Override
@@ -25,25 +42,153 @@ public class ProvidePhotoActivity extends Activity {
             finish();
         }
 
-//        Button btnTakePicture = findViewById(R.id.btnCapturePicture);
-//        btnTakePicture.setOnClickListener(v -> {
-//            if (CameraUtils.checkPerissions(getApplicationContext())) {
-//                captureImage();
-//            } else {
-//                requestCameraPermission();
-//            }
-//        });
-
-//        Button btnCamera = findViewById(R.id.btnCamera);
-//        btnCamera.setOnClickListener(v -> {
-//            Intent camera = new Intent(ProvidePhotoActivity.this, )
-//        });
+        Button btnTakePicture = findViewById(R.id.btnCapturePicture);
+        btnTakePicture.setOnClickListener(v -> {
+            if (CameraUtils.checkPermissions(getApplicationContext())) {
+                captureImage();
+            } else {
+                requestCameraPermission();
+            }
+        });
+        restoreFromBundle(savedInstanceState);
     }
 
+    /**
+     * Restoring store image path from saved instance state
+     */
+    private void restoreFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(CameraUtils.KEY_IMAGE_STORAGE_PATH)) {
+                storagePath = savedInstanceState.getString(CameraUtils.KEY_IMAGE_STORAGE_PATH);
+                if (!TextUtils.isEmpty(storagePath)) {
+                    if (storagePath.substring(storagePath.lastIndexOf(".")).equals("." + CameraUtils.IMAGE_EXTENSION)) {
+                        previewCapturedImage();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Requesting permissions using Dexter library
+     */
     private void requestCameraPermission() {
+        Dexter.withActivity(this)
+                .withPermissions(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                            captureImage();
+                        } else if (report.isAnyPermissionPermanentlyDenied()) {
+                            showPermissionsAlert();
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).check();
     }
 
-    private void captureImage() {
 
+    /**
+     * Capturing Camera Image will launch camera app requested image capture
+     */
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File file = CameraUtils.getOutputMediaFile();
+        if (file != null) {
+            storagePath = file.getAbsolutePath();
+        }
+
+        Uri fileUri = CameraUtils.getOutputMediaFileUri(getApplicationContext(), file);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    /**
+     * Saving stored image path to saved instance state
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save file url in bundle as it will be null on screen orientation
+        // changes
+        outState.putString(CameraUtils.KEY_IMAGE_STORAGE_PATH, storagePath);
+    }
+
+    /**
+     * Restoring image path from saved instance state
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // get the file url
+        storagePath = savedInstanceState.getString(CameraUtils.KEY_IMAGE_STORAGE_PATH);
+    }
+
+    /**
+     * Activity result method will be called after closing the camera
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            // Refreshing the gallery
+            CameraUtils.refreshGallery(getApplicationContext(), storagePath);
+
+            // successfully captured the image
+            // display it in image view
+            previewCapturedImage();
+        } else if (resultCode == RESULT_CANCELED) {
+            // user cancelled Image capture
+            Toast.makeText(getApplicationContext(),
+                    "User cancelled image capture", Toast.LENGTH_SHORT)
+                    .show();
+        } else {
+            // failed to capture image
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    /**
+     * Display image from gallery
+     */
+    private void previewCapturedImage() {
+        try {
+            TextView txtPreview = findViewById(R.id.txt_desc);
+            txtPreview.setVisibility(View.GONE);
+
+            ImageView imgPreview = findViewById(R.id.imgPreview);
+            imgPreview.setVisibility(View.VISIBLE);
+            Bitmap bitmap = CameraUtils.optimizeBitmap(CameraUtils.BITMAP_SAMPLE_SIZE, storagePath);
+            imgPreview.setImageBitmap(bitmap);
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Alert dialog to navigate to app settings
+     * to enable necessary permissions
+     */
+    private void showPermissionsAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissions required!")
+                .setMessage("Camera needs few permissions to work properly. Grant them in settings.")
+                .setPositiveButton("GOTO SETTINGS", (dialog, which) -> CameraUtils.openSettings(ProvidePhotoActivity.this))
+                .setNegativeButton("CANCEL", (dialog, which) -> {
+                }).show();
     }
 }
