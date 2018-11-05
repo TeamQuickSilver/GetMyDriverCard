d82e96020ceb9af5c3ac04ecc1836f5d5c02a63b
tevetely <cvetygetova@yahoo.com>
2018-11-05 22:47:51 +0200

    PreviewActivity
    Added some activities

---
+++ GetMyDriverCardAndroid/app/src/main/java/com/quicksilver/getmydrivercard/views/preview/PreviewActivity.java
@@ -0,0 +1,15 @@
+package com.quicksilver.getmydrivercard.views.preview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.quicksilver.getmydrivercard.R;
+
+public class PreviewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+    }
+}
---
+++ GetMyDriverCardAndroid/app/src/main/java/com/quicksilver/getmydrivercard/views/step3/PreviousCardPhotoActivity.java
@@ -0,0 +1,15 @@
+package com.quicksilver.getmydrivercard.views.step3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.quicksilver.getmydrivercard.R;
+
+public class PreviousCardPhotoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_card_photo);
+    }
+}
